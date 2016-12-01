7837e458a0fca0250c829e1e12479f5f5968c39d
tiagotrigo <tiago.a.trigo@gmail.com>
2016-12-01 13:35:29 -0300

    Delay para exibir mensagem de erro

--- dist/js/copaair-widgets.js
+++ dist/js/copaair-widgets.js
@@ -1814,6 +1814,7 @@ var Signup = function () {
       }).done(function () {
         // container.fadeOut();
         (0, _jquery2.default)('.copaair-widget-hidden').fadeOut();
+        (0, _jquery2.default)('.copaair-signup-message').delay(400).fadeIn();
         if (data.language == 'EN') {
           (0, _jquery2.default)('.copaair-signup-message').text('EN');
         } else if (data.language == 'PT') {
