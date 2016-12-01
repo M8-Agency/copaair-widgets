a35f1f51ccc6ed1737784730b138a52f4df2be0e
tiagotrigo <tiago.a.trigo@gmail.com>
2016-12-01 14:03:23 -0300

    trocando mensagem de erro

--- dist/js/copaair-widgets.js
+++ dist/js/copaair-widgets.js
@@ -1722,7 +1722,7 @@ var Signup = function () {
       $form.on('submit', function (e) {
         e.preventDefault();
         var err = true;
-        var messageErr = 'The fields marked with (*) are requiered.';
+        var messageErr = 'This field is required';
 
         if ((0, _jquery2.default)('.copaair-form-name').val() == '') {
           err = false;
